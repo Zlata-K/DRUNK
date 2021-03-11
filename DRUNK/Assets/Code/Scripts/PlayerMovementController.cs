d0641048c5234bdec9187e5bcee15a3f3a45a914
Edouard Gagne <tedgag@gmail.com>
2021-03-10 21:07:33 -0500

    Minor Cleanups

--- DRUNK/Assets/Code/Scripts/PlayerMovementController.cs
+++ DRUNK/Assets/Code/Scripts/PlayerMovementController.cs
@@ -6,108 +6,111 @@ namespace Code.Scripts
     // Controller class for the player to allow movement of the player model in the demo scene. (Mostly taken from Lab1)
     public class PlayerMovementController : MonoBehaviour
     {
+        public float acceleration = 1.5f;
+        public float decelerationFactor = 1.5f;
+        public float maxVelocity = 2.0f;
+        
         private static readonly int VelocityXHash = Animator.StringToHash("Velocity X");
         private static readonly int VelocityZHash = Animator.StringToHash("Velocity Z");
-        private float velocityX, velocityZ;
-        private float acceleration = 1.5f;
-        private float decelerationFactor = 1.5f;
-        private float maxVelocity = 2.0f;
-        private bool moveForward, moveBackward, moveLeft, moveRight, isRunning;
-        private GameObject plane;
-        private Animator animator;
+        
+        private float _velocityX, _velocityZ;
+        private bool _moveForward, _moveBackward, _moveLeft, _moveRight;
+        private GameObject _plane;
+        private Animator _animator;
 
         private const KeyCode MoveForwardKey = KeyCode.W;
         private const KeyCode MoveBackwardKey = KeyCode.S;
-        private const KeyCode RunKey = KeyCode.LeftShift;
         private const KeyCode MoveRightKey = KeyCode.D;
         private const KeyCode MoveLeftKey = KeyCode.A;
 
-        private Vector3 planeDimensions;
-        private Vector3 planePosition;
+        private Vector3 _planeDimensions;
+        private Vector3 _planePosition;
         
         void Awake()
         {
-            plane = GameObject.Find("Plane");
-            animator = GetComponent<Animator>();
-            var planeSize = plane.GetComponent<MeshFilter>().mesh.bounds.size;
-            var planeScaling = plane.transform.localScale;
-            planeDimensions = new Vector3(
+            _plane = GameObject.Find("Plane");
+            _animator = GetComponent<Animator>();
+            var planeSize = _plane.GetComponent<MeshFilter>().mesh.bounds.size;
+            var planeScaling = _plane.transform.localScale;
+            _planeDimensions = new Vector3(
                 planeSize.x * planeScaling.x,
                 planeSize.y * planeScaling.y,
                 planeSize.z * planeScaling.z);
-            planePosition = plane.transform.position;
+            _planePosition = _plane.transform.position;
         }
 
         private void VelocityUpdate()
         {
-            // Forward/Backward
-            if (moveForward && velocityZ < maxVelocity)
+            // Forward/Backward update
+            if (_moveForward && _velocityZ < maxVelocity)
             {
-                velocityZ += Time.deltaTime * acceleration;
+                _velocityZ += Time.deltaTime * acceleration;
             } 
-            else if (!moveForward && velocityZ > 0.0f)
+            else if (!_moveForward && _velocityZ > 0.0f)
             {
-                velocityZ -= Time.deltaTime * decelerationFactor  * acceleration;
+                _velocityZ -= Time.deltaTime * decelerationFactor  * acceleration;
             }
-            if (moveBackward && velocityZ > -maxVelocity)
+            if (_moveBackward && _velocityZ > -maxVelocity)
             {
-                velocityZ -= Time.deltaTime * acceleration;
+                _velocityZ -= Time.deltaTime * acceleration;
             } 
-            else if (!moveBackward  && velocityZ < 0.0f)
+            else if (!_moveBackward  && _velocityZ < 0.0f)
             {
-                velocityZ += Time.deltaTime * decelerationFactor  * acceleration;
+                _velocityZ += Time.deltaTime * decelerationFactor  * acceleration;
             }
-            // Left/Right
-            if (moveLeft && velocityX > -maxVelocity)
+            // Left/Right update
+            if (_moveLeft && _velocityX > -maxVelocity)
             {
-                velocityX -= Time.deltaTime * acceleration;
+                _velocityX -= Time.deltaTime * acceleration;
             }
-            else if (!moveLeft && velocityX < 0.0f)
+            else if (!_moveLeft && _velocityX < 0.0f)
             {
-                velocityX += Time.deltaTime * decelerationFactor  * acceleration;
+                _velocityX += Time.deltaTime * decelerationFactor  * acceleration;
             }
-            if (moveRight && velocityX < maxVelocity)
+            if (_moveRight && _velocityX < maxVelocity)
             {
-                velocityX += Time.deltaTime * acceleration;
+                _velocityX += Time.deltaTime * acceleration;
             }
-            else if (!moveRight && velocityX > 0.0f)
+            else if (!_moveRight && _velocityX > 0.0f)
             {
-                velocityX -= Time.deltaTime * decelerationFactor  * acceleration;
+                _velocityX -= Time.deltaTime * decelerationFactor  * acceleration;
             }
         }
 
         private void ConstrainVelocity()
         {
-            if (moveForward && velocityZ > maxVelocity)
+            //Forward/Backward constraints
+            if (_moveForward && _velocityZ > maxVelocity)
             {
-                velocityZ = maxVelocity;
+                _velocityZ = maxVelocity;
             }
-            if (moveBackward && velocityZ < -maxVelocity)
+            if (_moveBackward && _velocityZ < -maxVelocity)
             {
-                velocityZ = -maxVelocity;
+                _velocityZ = -maxVelocity;
             }
-            if (moveLeft && velocityX < -maxVelocity)
+            //Left/Right constraints
+            if (_moveLeft && _velocityX < -maxVelocity)
             {
-                velocityX = -maxVelocity;
+                _velocityX = -maxVelocity;
             }
-            if (moveRight && velocityX > maxVelocity)
+            if (_moveRight && _velocityX > maxVelocity)
             {
-                velocityX = maxVelocity;
+                _velocityX = maxVelocity;
             }
         }
         
         void Update()
         {
-            moveForward = Input.GetKey(MoveForwardKey);
-            isRunning = Input.GetKey(RunKey);
-            moveLeft = Input.GetKey(MoveLeftKey);
-            moveRight = Input.GetKey(MoveRightKey);
-            moveBackward = Input.GetKey(MoveBackwardKey);
+            _moveForward = Input.GetKey(MoveForwardKey);
+            _moveLeft = Input.GetKey(MoveLeftKey);
+            _moveRight = Input.GetKey(MoveRightKey);
+            _moveBackward = Input.GetKey(MoveBackwardKey);
+            
             VelocityUpdate();
             ConstrainVelocity();
             
-            animator.SetFloat(VelocityXHash, velocityX);
-            animator.SetFloat(VelocityZHash, velocityZ);
+            _animator.SetFloat(VelocityXHash, _velocityX);
+            _animator.SetFloat(VelocityZHash, _velocityZ);
             
             BoundCheck();
         }
@@ -118,13 +121,13 @@ namespace Code.Scripts
             var newPos = new Vector3();
             for (var i = 0; i < 3; i+=2)
             {
-                if (transform.position[i] < planePosition[i]-planeDimensions[i]/2.0f)
+                if (transform.position[i] < _planePosition[i]-_planeDimensions[i]/2.0f)
                 {
-                    newPos[i] = planePosition[i]+planeDimensions[i]/2.0f;
+                    newPos[i] = _planePosition[i]+_planeDimensions[i]/2.0f;
                 } 
-                else if (transform.position[i] > planePosition[i] + planeDimensions[i]/2.0f )
+                else if (transform.position[i] > _planePosition[i] + _planeDimensions[i]/2.0f )
                 {
-                    newPos[i] = planePosition[i]-planeDimensions[i]/2.0f;
+                    newPos[i] = _planePosition[i]-_planeDimensions[i]/2.0f;
                 }
                 else
                 {
