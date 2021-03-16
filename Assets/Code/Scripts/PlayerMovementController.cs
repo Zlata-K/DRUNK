b6c4e1b4f621e821a427008a3e1d0393d3ad3476
tedgag <tedgag@gmail.com>
2021-03-16 16:33:04 -0400

    Clean up movement controller and gitignore

--- Assets/Code/Scripts/PlayerMovementController.cs
+++ Assets/Code/Scripts/PlayerMovementController.cs
@@ -15,7 +15,6 @@ namespace Code.Scripts
         
         private float _velocityX, _velocityZ;
         private bool _moveForward, _moveBackward, _moveLeft, _moveRight;
-        private GameObject _plane;
         private Animator _animator;
 
         private const KeyCode MoveForwardKey = KeyCode.W;
@@ -23,20 +22,9 @@ namespace Code.Scripts
         private const KeyCode MoveRightKey = KeyCode.D;
         private const KeyCode MoveLeftKey = KeyCode.A;
 
-        private Vector3 _planeDimensions;
-        private Vector3 _planePosition;
-        
         void Awake()
         {
-            _plane = GameObject.Find("Plane");
             _animator = GetComponent<Animator>();
-            var planeSize = _plane.GetComponent<MeshFilter>().mesh.bounds.size;
-            var planeScaling = _plane.transform.localScale;
-            _planeDimensions = new Vector3(
-                planeSize.x * planeScaling.x,
-                planeSize.y * planeScaling.y,
-                planeSize.z * planeScaling.z);
-            _planePosition = _plane.transform.position;
         }
 
         private void VelocityUpdate()
@@ -98,45 +86,19 @@ namespace Code.Scripts
                 _velocityX = maxVelocity;
             }
         }
-        
+
         void Update()
         {
             _moveForward = Input.GetKey(MoveForwardKey);
             _moveLeft = Input.GetKey(MoveLeftKey);
             _moveRight = Input.GetKey(MoveRightKey);
             _moveBackward = Input.GetKey(MoveBackwardKey);
-            
+
             VelocityUpdate();
             ConstrainVelocity();
-            
+
             _animator.SetFloat(VelocityXHash, _velocityX);
             _animator.SetFloat(VelocityZHash, _velocityZ);
-            
-            BoundCheck();
-        }
-
-        // Places the player on the opposite side of the plane if they try to leave it.
-        private void BoundCheck()
-        {
-            var newPos = new Vector3();
-            for (var i = 0; i < 3; i+=2)
-            {
-                if (transform.position[i] < _planePosition[i]-_planeDimensions[i]/2.0f)
-                {
-                    newPos[i] = _planePosition[i]+_planeDimensions[i]/2.0f;
-                } 
-                else if (transform.position[i] > _planePosition[i] + _planeDimensions[i]/2.0f )
-                {
-                    newPos[i] = _planePosition[i]-_planeDimensions[i]/2.0f;
-                }
-                else
-                {
-                    newPos[i] = transform.position[i];
-                }
-            }
-
-            newPos.y = transform.position.y;
-            transform.position = newPos;
         }
     }
 }
