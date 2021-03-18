7422748f7b3d9c919ca0fa229bbff0d71d21aa31
Claudia <claudia.lapalme@gmail.com>
2021-03-18 18:13:06 -0400

    #5 - Implemented the main menu
    * Will implement the high score & art once it's available

---
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class MainMenuScript : MonoBehaviour
+    {
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        public void QuitGame()
+        {
+            Debug.Log("The game has been quit");
+            Application.Quit();
+        }
+    }
+}
