5d479af91aa3eba2ee37db87500457e8bd17d294
Claudia <claudia.lapalme@gmail.com>
2021-03-19 16:48:44 -0400

    #5 - Implemented high score table + addressed comments
    * Quit button leaves the unity editor
    * High score table shows up to 10 top scores
    * Moved the back button in the side menus & changed its colour for improved clarity
    * Removed useless TextMeshPro files and put all of the fonts in the TMP folder

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -1,10 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class MainMenuScript : MonoBehaviour
     {
+        // serializing this as it is the nicest way to fetch a deactivated game object
+        [SerializeField] private GameObject highScoreTextPanel;
+        
         public void PlayGame()
         {
             SceneManager.LoadScene(1);
@@ -12,8 +18,52 @@ namespace UI
 
         public void QuitGame()
         {
-            Debug.Log("The game has been quit");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
             Application.Quit();
+        #endif
+        }
+
+        public void SetHighScoreText()
+        {
+            var highScores = ScoreManager.GetScores();
+            var scoreTextBoxes = highScoreTextPanel.GetComponentsInChildren<TextMeshProUGUI>();
+            var index = 1;
+
+            ClearHighScores(scoreTextBoxes);
+            
+            //since the list of scores is sorted from smallest to biggest, we have to pull a good old switcheroo
+            for (var i = highScores.Count - 1; i >= 0; i--)
+            {
+                scoreTextBoxes[(int) HighScoreColumns.Positions].GetComponent<TextMeshProUGUI>()
+                    .text += index++ + "\n";
+
+                scoreTextBoxes[(int) HighScoreColumns.HighScores].GetComponent<TextMeshProUGUI>()
+                    .text += highScores[i].score + "\n";
+
+                scoreTextBoxes[(int) HighScoreColumns.PlayerNames].GetComponent<TextMeshProUGUI>()
+                    .text += highScores[i].name + "\n";
+
+                scoreTextBoxes[(int) HighScoreColumns.Dates].GetComponent<TextMeshProUGUI>()
+                    .text += highScores[i].date.ToString("d") + "\n";
+            }
+        }
+
+        private static void ClearHighScores(IEnumerable<TextMeshProUGUI> columns)
+        {
+            foreach (var column in columns)
+            {
+                column.text = "";
+            }
+        }
+
+        private enum HighScoreColumns
+        {
+            Positions = 0,
+            HighScores = 1,
+            PlayerNames = 2,
+            Dates = 3
         }
     }
 }
