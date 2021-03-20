d224763feb65d6edfc17d5a8ad4697b3f1b90415
Claudia <claudia.lapalme@gmail.com>
2021-03-19 20:17:36 -0400

    #4 - Implemented pause menu
    * stop time while in the pause menu
    * three buttons provided
       * play --> go back
       * high score --> see high score table
       * quit --> go back to the main menu

--- Assets/Scripts/UI/MainMenuScript.cs
+++ Assets/Scripts/UI/MainMenuScript.cs
@@ -6,11 +6,8 @@ using UnityEngine.SceneManagement;
 
 namespace UI
 {
-    public class MainMenuScript : MonoBehaviour
+    public class MainMenuScript : Menu
     {
-        // serializing this as it is the nicest way to fetch a deactivated game object
-        [SerializeField] private GameObject highScoreTextPanel;
-        
         public void PlayGame()
         {
             SceneManager.LoadScene(1);
@@ -29,43 +26,8 @@ namespace UI
         {
             var highScores = ScoreManager.GetScores();
             var scoreTextBoxes = highScoreTextPanel.GetComponentsInChildren<TextMeshProUGUI>();
-            var index = 1;
-
-            ClearHighScores(scoreTextBoxes);
             
-            //since the list of scores is sorted from smallest to biggest, we have to pull a good old switcheroo
-            for (var i = highScores.Count - 1; i >= 0; i--)
-            {
-                scoreTextBoxes[(int) HighScoreColumns.Positions].GetComponent<TextMeshProUGUI>()
-                    .text += index++ + "\n";
-
-                var format = new NumberFormatInfo {NumberGroupSeparator = " "};
-                
-                scoreTextBoxes[(int) HighScoreColumns.HighScores].GetComponent<TextMeshProUGUI>()
-                    .text += highScores[i].score.ToString("N0", format) + "\n";
-
-                scoreTextBoxes[(int) HighScoreColumns.PlayerNames].GetComponent<TextMeshProUGUI>()
-                    .text += highScores[i].name + "\n";
-
-                scoreTextBoxes[(int) HighScoreColumns.Dates].GetComponent<TextMeshProUGUI>()
-                    .text += highScores[i].date.ToString("d") + "\n";
-            }
-        }
-
-        private static void ClearHighScores(IEnumerable<TextMeshProUGUI> columns)
-        {
-            foreach (var column in columns)
-            {
-                column.text = "";
-            }
-        }
-
-        private enum HighScoreColumns
-        {
-            Positions = 0,
-            HighScores = 1,
-            PlayerNames = 2,
-            Dates = 3
+            SetHighScoreText(highScores, scoreTextBoxes);
         }
     }
 }
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class PauseMenu : Menu
+    {
+        [SerializeField] private GameObject pausePanel;
+
+        // Update is called once per frame
+        private void Update()
+        {
+            PauseGame();
+        }
+
+        public void SetHighScoreText()
+        {
+            var highScores = ScoreManager.GetScores();
+            var scoreTextBoxes = highScoreTextPanel.GetComponentsInChildren<TextMeshProUGUI>();
+            
+            SetHighScoreText(highScores, scoreTextBoxes);
+        }
+        
+        private void PauseGame()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                pausePanel.SetActive(true);
+            }
+        }
+
+        public void UnpauseGame()
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+
+        public void QuitGame()
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+            SceneManager.LoadScene(0);
+        }
+    }
+}
