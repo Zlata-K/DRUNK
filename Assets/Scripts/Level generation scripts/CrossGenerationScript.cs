d3ce9c1cb7991964b09dc12151f263f9a00a5dc6
Evan Dimopoulos <evan.dime@gmail.com>
2021-03-23 16:57:28 -0400

    crossroads now check for edge pieces

--- Assets/Scripts/Level generation scripts/CrossGenerationScript.cs
+++ Assets/Scripts/Level generation scripts/CrossGenerationScript.cs
@@ -5,10 +5,74 @@ using UnityEngine;
 
 public class CrossGenerationScript : TileGenerationScript
 {
+    private int? _area = null;
+
+    //set as crossroad
     public override GameObject Generate(int column, int row)
     {
-        //set as crossroad
-        //TODO::add logic to check if the crossroad is a map edge or corner
-        return Instantiate(tiles[Random.Range(0, tiles.Count)], new Vector3(column * Width, 0, row * Width), Quaternion.identity);
+        if (_area == null)
+        {
+            _area = ((Size - 1) * Blocks + 1) - 1;
+        }
+
+        //Bottom left corner
+        if (row == 0 && column == 0)
+        {
+            return Instantiate(tiles[0], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, -90, 0));
+        }
+        
+        //top left corner
+        if (row == _area && column == 0)
+        {
+            return Instantiate(tiles[0], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 0, 0));
+        }
+        
+        //top right corner
+        if (row == _area && column == _area)
+        {
+            return Instantiate(tiles[0], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 90, 0));
+        }
+        
+        //bottom right corner
+        if (row == 0 && column == _area)
+        {
+            return Instantiate(tiles[0], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 180, 0));
+        }
+
+        //bottom row
+        if (row == 0)
+        {
+            return Instantiate(tiles[1], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 180, 0));
+        }
+        
+        //top row
+        if (row == _area)
+        {
+            return Instantiate(tiles[1], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 0, 0));
+        }
+
+        //left column
+        if (column == 0)
+        {
+            return Instantiate(tiles[1], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, -90, 0));
+        }
+        
+        //right column
+        if (column == _area)
+        {
+            return Instantiate(tiles[1], new Vector3(column * Width, 0, row * Width),
+                Quaternion.Euler(0, 90, 0));
+        }
+        
+        //default - we are not along an edge
+        return Instantiate(tiles[2], new Vector3(column * Width, 0, row * Width),
+            Quaternion.identity);
     }
 }
