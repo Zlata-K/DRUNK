d5288fa3c3b0ef9d78c48587fa3764b6b70fce8e
Evan Dimopoulos <evan.dime@gmail.com>
2021-04-01 13:58:54 -0400

    Quick add of interactable player can run through and it will trigger at the moment it does not reset

---
+++ Assets/Scripts/Level generation scripts/TriggerInteractableScript.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerInteractableScript : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private static readonly int Activated = Animator.StringToHash("Activated");
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name.ToLower().Contains("player"))
+        {
+            _animator.SetTrigger(Activated);
+        }
+    }
+}
