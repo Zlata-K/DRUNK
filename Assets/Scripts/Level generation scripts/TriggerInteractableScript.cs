9afde8050f179a34872ec0b6ae94ca63cfbbf11e
Evan Dimopoulos <evan.dime@gmail.com>
2021-04-01 14:20:53 -0400

    Added timer to reset obstacles also add random flip for roads

--- Assets/Scripts/Level generation scripts/TriggerInteractableScript.cs
+++ Assets/Scripts/Level generation scripts/TriggerInteractableScript.cs
@@ -7,12 +7,26 @@ public class TriggerInteractableScript : MonoBehaviour
 {
     [SerializeField] private Animator _animator;
     private static readonly int Activated = Animator.StringToHash("Activated");
-
+    [SerializeField] private float cooldown;
+    private float timer = 0;
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.name.ToLower().Contains("player"))
         {
-            _animator.SetTrigger(Activated);
+            _animator.SetBool(Activated, true);
+            timer = 0.0f;
+        }
+    }
+
+    private void Update()
+    {
+        if (_animator.GetBool(Activated))
+        {
+            if (timer >= cooldown)
+            {
+                _animator.SetBool(Activated, false);
+            }
+            timer += Time.deltaTime;
         }
     }
 }
