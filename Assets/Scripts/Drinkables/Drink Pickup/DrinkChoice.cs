9b4b58116a653b7567535dd35ea1fbf52109eebe
Zlata K <zlata.098@gmail.com>
2021-04-22 13:36:20 -0400

    Changed trees' colliders & shadows + some pickup menu WIP

---
+++ Assets/Scripts/Drinkables/Drink Pickup/DrinkChoice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Drinkables;
+using UnityEngine;
+
+public class DrinkChoice : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject buffaloDrink;
+    [SerializeField] private GameObject clearlyDrink;
+    [SerializeField] private GameObject flippedDrink;
+    [SerializeField] private float timeLimit;
+    //[SerializeField] private GameObject water;
+    
+    public bool isActive;
+    private float timeCounter = 0;
+    
+    void Update()
+    {
+        if (isActive)
+        {
+            Time.timeScale = 0;
+            menuPanel.SetActive(isActive);
+            timeCounter += Time.unscaledDeltaTime;
+
+            if (timeCounter > timeLimit)
+            {
+                isActive = false;
+                menuPanel.SetActive(isActive);
+                Time.timeScale = 1;
+                timeCounter = 0;
+            }
+        }
+    }
+
+    public void ConsumeBuffalo()
+    {
+        isActive = false;
+        menuPanel.SetActive(isActive);
+        Time.timeScale = 1;
+        Instantiate(buffaloDrink);
+    }
+
+    public void ConsumeClearlyThere()
+    {
+        isActive = false;
+        menuPanel.SetActive(isActive);
+        Time.timeScale = 1;
+        Instantiate(clearlyDrink);
+    }
+
+    public void ConsumeUpsideDown()
+    {
+        isActive = false;
+        menuPanel.SetActive(isActive);
+        Time.timeScale = 1;
+        Instantiate(flippedDrink);
+    }
+    
+}
---
+++ Assets/Scripts/Drinkables/Drink Pickup/TavernEntrance.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TavernEntrance : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject.Find("DrinkChoiceCanvas").GetComponent<DrinkChoice>().isActive = true;
+        }
+    }
+}
