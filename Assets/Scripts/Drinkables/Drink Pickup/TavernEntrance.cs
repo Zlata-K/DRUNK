11eca8eef952b71c57c49e9f91a7f9e8b997d180
Evan Dimopoulos <evan.dime@gmail.com>
2021-04-22 18:34:27 -0400

    Added cooldown for tavern pickup. may add indicator later

--- Assets/Scripts/Drinkables/Drink Pickup/TavernEntrance.cs
+++ Assets/Scripts/Drinkables/Drink Pickup/TavernEntrance.cs
@@ -1,13 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TavernEntrance : MonoBehaviour
 {
+    [SerializeField] private float tavernCoolDown = 30;
+
+    private bool tavernOpen;
+    private float cooldown;
+
+    private void Start()
+    {
+        tavernOpen = true;
+        cooldown = 0;
+    }
+
+    private void Update()
+    {
+        if (!tavernOpen)
+        {
+            cooldown += Time.deltaTime;
+            if (cooldown > tavernCoolDown)
+            {
+                tavernOpen = true;
+                cooldown = 0;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (tavernOpen && other.CompareTag("Player"))
         {
+            tavernOpen = false;
             GameObject.Find("DrinkChoiceCanvas").GetComponent<DrinkChoice>().EnterTheBar();
         }
     }
