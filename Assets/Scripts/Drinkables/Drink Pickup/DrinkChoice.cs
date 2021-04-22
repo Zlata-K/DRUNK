efd63e2f03a1dff7cefd3f63101ccb49a694b0d5
Zlata K <zlata.098@gmail.com>
2021-04-22 17:53:30 -0400

    Randomized drinks

--- Assets/Scripts/Drinkables/Drink Pickup/DrinkChoice.cs
+++ Assets/Scripts/Drinkables/Drink Pickup/DrinkChoice.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Drinkables;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+//using Random = System.Random;
 
 public class DrinkChoice : MonoBehaviour
 {
@@ -10,11 +14,17 @@ public class DrinkChoice : MonoBehaviour
     [SerializeField] private GameObject buffaloDrink;
     [SerializeField] private GameObject clearlyDrink;
     [SerializeField] private GameObject flippedDrink;
+    [SerializeField] private GameObject waterDrink;
+    [SerializeField] private GameObject beerDrink;
     [SerializeField] private float timeLimit;
-    //[SerializeField] private GameObject water;
+    [SerializeField] private GameObject buffaloButton;
+    [SerializeField] private GameObject clearlyButton;
+    [SerializeField] private GameObject flippedButton;
     
+    private int randomDrink;
     public bool isActive;
     private float timeCounter = 0;
+    private bool randomIsChosen;
     
     void Update()
     {
@@ -22,15 +32,42 @@ public class DrinkChoice : MonoBehaviour
         {
             Time.timeScale = 0;
             menuPanel.SetActive(isActive);
+            GenerateRandomDrink();
             timeCounter += Time.unscaledDeltaTime;
 
             if (timeCounter > timeLimit)
             {
                 isActive = false;
                 menuPanel.SetActive(isActive);
+                clearlyButton.SetActive(false);
+                buffaloButton.SetActive(false);
+                flippedButton.SetActive(false);
                 Time.timeScale = 1;
                 timeCounter = 0;
+                randomIsChosen = false;
             }
+        }
+    }
+
+    private void GenerateRandomDrink()
+    {
+        if (!randomIsChosen)
+        {
+            randomDrink = Random.Range(0, 3);
+            if (randomDrink == 0)
+            {
+                buffaloButton.SetActive(true);
+            }
+            else if (randomDrink == 1)
+            {
+                clearlyButton.SetActive(true);
+            }
+            else
+            {
+                flippedButton.SetActive(true);
+            }
+
+            randomIsChosen = true;
         }
     }
 
@@ -38,24 +75,60 @@ public class DrinkChoice : MonoBehaviour
     {
         isActive = false;
         menuPanel.SetActive(isActive);
+        clearlyButton.SetActive(false);
+        buffaloButton.SetActive(false);
+        flippedButton.SetActive(false);
         Time.timeScale = 1;
         Instantiate(buffaloDrink);
+        randomIsChosen = false;
     }
 
     public void ConsumeClearlyThere()
     {
         isActive = false;
         menuPanel.SetActive(isActive);
+        clearlyButton.SetActive(false);
+        buffaloButton.SetActive(false);
+        flippedButton.SetActive(false);
         Time.timeScale = 1;
         Instantiate(clearlyDrink);
+        randomIsChosen = false;
     }
 
     public void ConsumeUpsideDown()
     {
         isActive = false;
         menuPanel.SetActive(isActive);
+        clearlyButton.SetActive(false);
+        buffaloButton.SetActive(false);
+        flippedButton.SetActive(false);
         Time.timeScale = 1;
         Instantiate(flippedDrink);
+        randomIsChosen = false;
+    }
+
+    public void ConsumeWater()
+    {
+        isActive = false;
+        menuPanel.SetActive(isActive);
+        clearlyButton.SetActive(false);
+        buffaloButton.SetActive(false);
+        flippedButton.SetActive(false);
+        Time.timeScale = 1;
+        Instantiate(waterDrink);
+        randomIsChosen = false;
+    }
+
+    public void ConsumeBeer()
+    {
+        isActive = false;
+        menuPanel.SetActive(isActive);
+        clearlyButton.SetActive(false);
+        buffaloButton.SetActive(false);
+        flippedButton.SetActive(false);
+        Time.timeScale = 1;
+        Instantiate(beerDrink);
+        randomIsChosen = false;
     }
     
 }
