e1708088fbd5ffa4b68d4371033984b904f99532
Edouard Gagne <tedgag@gmail.com>
2021-04-24 19:21:09 -0400

    Added input field and score save on submit

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,12 @@ namespace UI
     public class GameOverMenu : Menu
     {
         [SerializeField] private TMP_Text scoreText;
+        [SerializeField] private GameObject scoreInput;
+        [SerializeField] private TMP_Text inputText;
+        [SerializeField] private GameObject submitSuccessText;
+
+        private bool _scoreSubmitted;
+        
         public void Awake()
         {
             Cursor.lockState = CursorLockMode.None;
@@ -15,12 +22,43 @@ namespace UI
         }
         public void PlayAgain()
         {
+            ScoreManager.AddScore("Player", Indestructibles.PlayerData.CurrentScore);
             SceneManager.LoadScene(1);
         }
         
         public void Quit2MainMenu()
         {
+            ScoreManager.AddScore("Player", Indestructibles.PlayerData.CurrentScore);
             SceneManager.LoadScene(0);
+        }
+
+        public void OnScoreSubmit()
+        {
+            _scoreSubmitted = true;
+            scoreInput.SetActive(false);
+            submitSuccessText.SetActive(true);
+            if (inputText.text != "")
+            {
+                ScoreManager.AddScore(inputText.text, Indestructibles.PlayerData.CurrentScore);
+            }
+            else
+            {
+                ScoreManager.AddScore("Player", Indestructibles.PlayerData.CurrentScore);
+            }
+          
+        }
+
+        private void OnEnable()
+        {
+            submitSuccessText.SetActive(false);
+            if (_scoreSubmitted)
+            {
+                scoreInput.SetActive(false);
+            }
+            else
+            {
+                scoreInput.SetActive(true);
+            }
         }
     }
 }
